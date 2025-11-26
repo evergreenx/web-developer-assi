45fb7c06949633b38fd4bdc4c73e5fec1d75c026
evergreenx <dicksonevergreenido@gmail.com>
2025-11-27 00:42:40 +0100

    feat: add delete post by ID to API

--- apps/backend/src/db/posts/posts.ts
+++ apps/backend/src/db/posts/posts.ts
@@ -1,5 +1,5 @@
 import { connection } from "../connection";
-import { selectPostsTemplate } from "./query-tamplates";
+import { selectPostsTemplate, deletePostTemplate } from "./query-tamplates";
 import { Post } from "./types";
 
 export const getPosts = (userId: string): Promise<Post[]> =>
@@ -9,5 +9,15 @@ export const getPosts = (userId: string): Promise<Post[]> =>
         reject(error);
       }
       resolve(results as Post[]);
+    });
+  });
+
+export const deletePost = (postId: string): Promise<number> =>
+  new Promise((resolve, reject) => {
+    connection.run(deletePostTemplate, [postId], function (error) {
+      if (error) {
+        reject(error);
+      }
+      resolve(this.changes);
     });
   });
--- apps/backend/src/routes/posts.ts
+++ apps/backend/src/routes/posts.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { getPosts } from "../db/posts/posts";
+import { getPosts, deletePost } from "../db/posts/posts";
 
 const router = Router();
 
@@ -11,6 +11,24 @@ router.get("/", async (req: Request, res: Response) => {
   }
   const posts = await getPosts(userId);
   res.send(posts);
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  const postId = req.params.id;
+  if (!postId) {
+    res.status(400).send({ error: "postId is required" });
+    return;
+  }
+  try {
+    const changes = await deletePost(postId);
+    if (changes === 0) {
+      res.status(404).send({ message: "Post not found" });
+    } else {
+      res.status(200).send({ message: "Post deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).send({ error: "Error deleting post" });
+  }
 });
 
 export default router;
