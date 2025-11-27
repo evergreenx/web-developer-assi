d6bfeee860f09461408b737ee3a165e7b2fa7721
evergreenx <dicksonevergreenido@gmail.com>
2025-11-27 22:29:56 +0100

    fix build error

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import UserPosts from "./components/user-posts";
